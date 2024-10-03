a298d20ae760278128ea501ad18a9947abfccfe1
Danil Plokhikh laptop <danil.plokhikh@gmail.com>
2024-10-03 10:49:06 +0300

    model order created, migrations created

--- webapp/orders/models.py
+++ webapp/orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    title = models.CharField('Название', max_length=50),
+    description = models.CharField('Описание', max_length=250)
+    datetime = models.DateTimeField('Дата и время')
+    # currency_crypto: = ""
+    # currency_fiat: = ""
+    # amount_crypto: = ""
+    # amount_fiat: = ""
+    # status: = ""
+    # goal: = ""
+
+def __str__(self):
+    return self.title
